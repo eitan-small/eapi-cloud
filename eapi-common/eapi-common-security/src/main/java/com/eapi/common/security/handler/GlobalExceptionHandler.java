3cfe2b9493c9d83fd1a8c7665f85d7cc3328e135
eitan <18101612160@163.com>
2023-10-23 19:28:32 +0800

    first commit

---
+++ eapi-common/eapi-common-security/src/main/java/com/eapi/common/security/handler/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.eapi.common.security.handler;
+
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author ruoyi
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
